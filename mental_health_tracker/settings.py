fb73da7dd6e4dada0b39b7f70e392ab30c575546
zufarra <zufarromli@gmail.com>
2024-08-28 16:26:06 +0700

    selesai melakukan

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3^_80isu@bt*jz($6gpcw7w&()ky=zgk%aaf$zg7vf=dtvh4v!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "zufar-romli-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
