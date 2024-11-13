0b80e73798d92225032c895e129294f799261e5e
zufarra <zufarromli@gmail.com>
2024-11-13 16:46:50 +0700

    menyelesaikan tutorial 8

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from authentication.views import login, register
+from authentication.views import login, register, logout
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
 
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 import json
+from django.contrib.auth import logout as auth_logout
+
 
 @csrf_exempt
 def login(request):
@@ -68,3 +70,20 @@ def register(request):
             "status": False,
             "message": "Invalid request method."
         }, status=400)
+    
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logout berhasil!"
+        }, status=200)
+    except:
+        return JsonResponse({
+        "status": False,
+        "message": "Logout gagal."
+        }, status=401)
